38f6f77216a9c00714a42903d24249c69dc7d7c2
cloudera <cloudera@quickstart.cloudera>
2017-12-31 02:47:13 -0800

    Reorganización diversa de ficheros

--- Varios/EjemplosMapreduceHive/examplehive/mapper_clean.py
+++ Varios/EjemplosMapreduceHive/examplehive/mapper_clean.py
@@ -10,5 +10,3 @@ for line in sys.stdin:
 
     
     print "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\t{10}\t{11}\t{12}".format(idref, ident, gsm19023, gsd19024, gsd19025, gsd19026, genetitle, genesymbol, geneID, uniGenetitle, uniGenesymbol, uniGeneID, NucleotideTitle)
-
- 
